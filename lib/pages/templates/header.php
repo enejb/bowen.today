63e00c0bf3951f98595a76f73813b76e6ae5727c
Enej Bajgoric <enej.bajgoric@gmail.com>
2021-01-18 22:05:04 -0800

    Load the minified style

--- lib/pages/templates/header.php
+++ lib/pages/templates/header.php
@@ -5,6 +5,6 @@
   <title><?php echo $title; ?></title>
   <meta name="description" content="<?php echo $title; ?>">
   <meta name="viewport" content="width=device-width, initial-scale=1.0" >
-  <link rel="stylesheet" href="/style.css">
+  <link rel="stylesheet" href="/style.min.css">
 </head>
 <body>
