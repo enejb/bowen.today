aa38e0c96e3f459561382ce1a9db068e88c58082
Enej Bajgoric <enej.bajgoric@gmail.com>
2021-02-10 21:19:06 -0800

    DRY: include the header from home

--- pages/templates/header.php
+++ pages/templates/header.php
@@ -1,16 +1,5 @@
-<!doctype html>
-<html lang="en">
+<?php require 'header-home.php'; ?>
 
-<head>
-    <meta charset="utf-8">
-    <title><?php echo $title; ?></title>
-    <meta name="description" content="<?php echo $title; ?>">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/style.css">
-</head>
-<!-- If you are reading this checkout the project at github.com/enejb/bowen.today -->
-
-<body>
     <div class="header">
         <div class="header-shell">
             <a class="btn" href="/"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width="16" height="16"
